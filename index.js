e3fa23824b53fdd763c563a6f12bb0874adffac0
Hu Ziming <hzmangel@gmail.com>
2016-05-18 03:04:17 +0800

    Start script of server

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var koa = require('koa');
+var router = require('./app/routes');
+var logger = require('koa-logger');
+var bodyParser = require('koa-bodyparser');
+
+var app = koa();
+
+app.use(logger());
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+require('koa-validate')(app);
+
+app.on('error', function(err,ctx){
+    if (process.env.NODE_ENV != 'test') {
+        console.log(err.message);
+        console.log(err);
+    }
+});
+
+app.listen(3000);
+
