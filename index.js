a16610537dd088fc0ea0172b086afcd54053a932
Hu Ziming <hzmangel@gmail.com>
2016-05-18 16:17:56 +0800

    Add DB related code, and change code order
    After splitting code into several files, the importing and initzlization order should be changed to make files working.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var koa = require('koa');
-var router = require('./app/routes');
+var koaPg = require('koa-pg')
+var Router = require('koa-router');
 var logger = require('koa-logger');
 var bodyParser = require('koa-bodyparser');
 
-var app = koa();
+var app = module.exports = koa();
+var config = require('./config');
 
 app.use(logger());
 app.use(bodyParser());
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(koaPg(config.pg_connect_str));
 
 require('koa-validate')(app);
+
+var ctrl = require('./app/controllers')
+var router = require('./app/routes')
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.on('error', function(err,ctx){
   if (process.env.NODE_ENV != 'test') {
