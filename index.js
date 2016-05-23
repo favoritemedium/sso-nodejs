613ca78e57f02d7f07c993f9b1b39c5581c960f7
Hu Ziming <hzmangel@gmail.com>
2016-05-24 01:44:44 +0800

    Reorder code, and merge routers to this file

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 'use strict';
 
 var koa = require('koa');
-var koaPg = require('koa-pg')
 var Router = require('koa-router');
 var logger = require('koa-logger');
+var mongoose = require('mongoose');
+var passport = require('koa-passport');
 var bodyParser = require('koa-bodyparser');
 
+
 var app = module.exports = koa();
-var config = require('./config');
+var config = require('./config/app');
+var ctrl = require('./app/controllers')
 
 app.use(logger());
 app.use(bodyParser());
-app.use(koaPg(config.pg_connect_str));
 
 require('koa-validate')(app);
+require('./config/passport')(passport, config);
 
-var ctrl = require('./app/controllers')
-var router = require('./app/routes')
+// Connect to DB
+mongoose.connect(config.mongo_connect_str);
+mongoose.connection.on('error', function (err) {
+    console.log(err);
+});
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+const EmailAuth = require('./app/models/email_auth.js')
+
+
+// Routes
+var router = new Router({
+  prefix: '/api/auth'
+});
+
+router.post('/auth', passport.authenticate('local', {
+  failureRedirect: '/api/auth/auth',
+  session:false
+}), function*(next) {
+  this.body = "LOGGED IN as " + this.req.user.id;
+  //this.redirect("/api/auth/success?id=" + this.req.user._id);
+});
+
+router.get('/success',
+  //passport.authenticate('bearer', { session: false }),
+  function *(next) {
+    console.log(this.req)
+    this.body = "LOGGED IN as " + this.req.user.id;
+  }
+);
+
 
 app.on('error', function(err,ctx){
   if (process.env.NODE_ENV != 'test') {
@@ -27,6 +55,12 @@ app.on('error', function(err,ctx){
     console.log(err);
   }
 });
+
+app.use(passport.initialize());
+app.use(router.routes());
+//app.use(router.middleware());
+app.use(router.allowedMethods());
+
 
 app.listen(3000);
 
