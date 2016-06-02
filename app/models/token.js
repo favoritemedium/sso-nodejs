990046226a54f59a363412257e37cb7d7623cc11
Hu Ziming <hzmangel@gmail.com>
2016-06-02 23:49:51 +0800

    Format code

--- app/models/token.js
+++ app/models/token.js
@@ -7,25 +7,45 @@ var config = require('../../config/app')
 
 // Token used for this SSO site
 var TokenSchema = new Schema({
-  member_id: { type: Schema.ObjectId, required: true },
-  access_token: { type: String, required: true },
-  access_token_expire_at: { type: Number, required: true },
-  refresh_token: { type: String, required: true },
-  refresh_token_expire_at: { type: Number, required: true },
+  member_id: {
+    type: Schema.ObjectId,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  access_token: {
+    type: String,
+    required: true
+  },
+  access_token_expire_at: {
+    type: Number,
+    required: true
+  },
+  refresh_token: {
+    type: String,
+    required: true
+  },
+  refresh_token_expire_at: {
+    type: Number,
+    required: true
+  },
 });
 
-TokenSchema.methods.reload_tokens = function *(candidate_pwd) {
+TokenSchema.methods.reloadTokens = function* (candidate_pwd) {
   var current_epoch_seconds = new Date().getTime() / 1000;
 
   if (this.access_token_expire_at < current_epoch_seconds) {
     this.access_token_expire_at = current_epoch_seconds + config.access_token_expire_second;
-    this.access_token = generate_token();
+    this.access_token = generateToken();
     yield this.save();
   }
 
   if (this.refresh_token_expire_at < current_epoch_seconds) {
     this.refresh_token_expire_at = current_epoch_seconds + config.refresh_token_expire_second;
-    this.refresh_token = generate_token();
+    this.refresh_token = generateToken();
     yield this.save();
   }
 
@@ -37,16 +57,26 @@ TokenSchema.methods.reload_tokens = function *(candidate_pwd) {
   }
 }
 
-var rand = function() {
+TokenSchema.methods.member = function* () {
+  return yield Member.findById(this.member_id);
+}
+
+TokenSchema.methods.createMember = function* () {
+  var member = yield Member.create({});
+  this.member_id = member.id;
+  yield this.save();
+  return member;
+}
+
+var rand = function () {
   // Generate base 36 (0-9a-z) random number, and use substr to remove `0.`
   return Math.random().toString(36).substr(2);
 }
 
-var generate_token = function() {
+var generateToken = function () {
   return rand() + rand();
 }
 
 module.exports = mongoose.model('Token', TokenSchema);
 
 // vim: expandtab:ts=2:sw=2
-
