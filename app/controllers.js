17f3929f9fe185f7fe29b42dd2a08c902f51c00b
Hu Ziming <hzmangel@gmail.com>
2016-06-02 23:54:05 +0800

    Put controller code from index to here

--- app/controllers.js
+++ app/controllers.js
@@ -1,86 +1,130 @@
-var ctrl = {};
+var config = require('../config/app');
 
-ctrl.auth = function *(next) {
-  // Pass email
-  this.checkBody('email').optional().isEmail('Please enter correct email');
-  this.checkBody('password').optional().notEmpty();
+const EmailAuth = require('./models/email_auth')
+const Token = require('./models/token')
+const SnsAuth = require('./models/sns_auth')
+const Member = require('./models/member')
 
-  // Pass refresh token
-  this.checkBody('rtoken').optional().notEmpty();
+module.exports = function (app, passport) {
+  var ctrl = {};
 
-  // Pass provider
-  this.checkBody('provider').optional().notEmpty();
-  this.checkBody('id_token').optional().notEmpty();
+  ctrl.authParams = function* (next) {
+    // Pass email
+    this.checkBody('email').optional().isEmail('Please enter correct email');
+    this.checkBody('password').optional().notEmpty();
 
-  if (this.errors) {
-    this.body = this.errors;
-    return
+    // Pass refresh token
+    this.checkBody('rtoken').optional().notEmpty();
+
+    // Pass provider
+    this.checkBody('provider').optional().notEmpty();
+    this.checkBody('id_token').optional().notEmpty();
+
+    if (this.errors) {
+      this.body = this.errors;
+      return
+    }
+
+    var email = this.checkBody('email').value;
+    var password = this.checkBody('password').value;
+    var rtoken = this.checkBody('rtoken').value;
+    var provider = this.checkBody('provider').value;
+    var id_token = this.checkBody('id_token').value;
+
+    if ((typeof email !== 'undefined') && (typeof password !== 'undefined')) {
+      // Find by email and password
+      var email_owner = yield EmailAuth.matchRecord(email, password);
+      if (!email_owner) {
+        return this.status = 401;
+      }
+
+      this.user = email_owner;
+      this.signin_method = 'email';
+      yield next;
+    } else if (typeof rtoken !== 'undefined') {
+      // Find by refresh token
+      var token_owner = yield Tokens.findOne({
+        refresh_token: rtoken,
+        disabled: false
+      });
+
+      if (!token_owner) {
+        // TODO: Log the error
+        return this.status = 401;
+      }
+
+      this.user = token_owner;
+      this.signin_method = 'refresh_token';
+      yield next;
+    } else if ((typeof provider !== 'undefined') && (typeof id_token !== 'undefined')) {
+      // Find by provider and id_token
+      var sns_owner = yield SnsAuth.matchRecord(provider, id_token);
+      if (!sns_owner) {
+        // TODO: Log the error
+        return this.status = 401;
+      }
+
+      this.user = sns_owner;
+      this.signin_method = 'sns';
+      yield next;
+    } else {
+      this.body = {
+        'error': 'Params error!'
+      }
+      return
+    }
   }
 
-  var email = this.checkBody('email').value;
-  var password = this.checkBody('password').value;
-  var rtoken = this.checkBody('rtoken').value;
-  var provider = this.checkBody('provider').value;
-  var id_token = this.checkBody('id_token').value;
-
-  if ((typeof email !== 'undefined') && (typeof password !== 'undefined')) {
-    if (auth_by_email_password(email, password)) {
-      signin(email, password);
-    } else {
-      // TODO: Return error
-    }
-  } else if (typeof rtoken !== 'undefined') {
-    if (auth_by_refresh_token(rtoken)) {
-    } else {
-    }
-    // TODO: Signin user, generate tokens and return ok if verified
-    // TODO: Return error if not verified
-  } else if ((typeof provider !== 'undefined') && (typeof id_token !== 'undefined')) {
-    if (auth_by_provider_token(rtoken)) {
-    } else {
-    }
-  } else {
-    console.log('----------');
-    this.body = { 'error': 'Params error!' }
-    return
+  ctrl.updateSession = function* (next) {
+    console.log('Update session');
+    yield next;
   }
+
+  ctrl.returnMemberInfo = function* (next) {
+    var member = yield this.user.member();
+    if (!member) {
+      if (this.signin_method !== 'refresh_token') {
+        this.body = {
+          email: this.user.email,
+          name: this.user.name,
+          new_member: true
+        }
+      } else {
+        member = yield this.user.createMember();
+        var tokens = yield this.user.refreshTokens();
+        this.body = Object.assign({}, member.toJSON(), tokens);
+      }
+    }
+  }
+
+  ctrl.signOut = function *() {
+    this.checkBody('access_token').optional();
+    // TODO: Find session and clear it
+    var access_token = this.checkBody('access_token').value;
+    if (typeof access_token !== 'undefined') {
+      yield Token.update({access_token: access_token}, {disabled: true});
+      this.body = 'ok'
+    }
+  }
+
+  ctrl.verify = function* (next) {
+    var access_token = this.request.body.access_token;
+    var member = yield Member.findByToken(access_token);
+
+    // TODO: Handle member not found correctly
+    if (member) {
+      this.body = member.toJSON();
+    } else {
+      this.body = 'aaaa';
+    }
+  }
+
+  ctrl.hello = function* (next) {
+    this.body = 'Hello'
+  }
+
+
+  return ctrl;
 }
-
-ctrl.hello = function *(next) {
-  console.log(this.pg);
-  var result = yield this.pg.db.client.query_('SELECT now()');
-  console.log('result:', result);
-
-  this.body = result.rows[0].now.toISOString()
-}
-
-/////////////////////////////
-// Help functions
-/////////////////////////////
-
-// Auth by email and password
-auth_by_email_password = function(email, password) {
-  // TODO: Query EmailAuth table with email and password
-  return true;
-}
-
-// Auth by refresh token
-auth_by_refresh_token = function(refresh_token) {
-  // TODO: Find refresh token in Token table
-  return true;
-}
-
-// Auth by provider and id_token
-auth_by_provider_token = function(provider, id_token) {
-  // TODO: Find refresh token in SnsAuth table
-  return true;
-}
-
-signin = function(email, password) {
-  // TODO: Signin user, generate tokens
-}
-
-module.exports = ctrl;
 
 // vim: expandtab:ts=2:sw=2
-
