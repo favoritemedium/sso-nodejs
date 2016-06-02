8f8da8aafd2daba60bdb4a1b36ca50738615f22d
Hu Ziming <hzmangel@gmail.com>
2016-06-02 23:53:00 +0800

    Add member related functions

--- app/models/sns_auth.js
+++ app/models/sns_auth.js
@@ -1,17 +1,72 @@
 'use strict';
 
+var config = require('../../config/app');
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SnsAuthSchema = new Schema({
-  provider: { type: String, required: true },
-  id_token: { type: String, required: true },
-  access_token: { type: String, required: true },
-  access_token_expire_at: { type: Integer, required: true },
-  refresh_token: { type: String, required: true },
-  refresh_token_expire_at: { type: Integer, required: true },
+  member_id: {
+    type: Schema.ObjectId,
+    required: true
+  },
+  provider: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  id_token: {
+    type: String,
+    required: true
+  },
+  access_token: {
+    type: String,
+    required: true
+  },
+  access_token_expire_at: {
+    type: Number,
+    required: true
+  },
+  refresh_token: {
+    type: String,
+    required: true
+  },
+  refresh_token_expire_at: {
+    type: Number,
+    required: true
+  },
 });
+
+SnsAuthSchema.methods.member = function* () {
+  return yield Member.findById(this.member_id);
+}
+
+SnsAuthSchema.methods.createMember = function* () {
+  var member = yield Member.create({});
+  this.member_id = member.id;
+  yield this.save();
+  return member;
+}
+
+SnsAuthSchema.statics.matchRecord = function* (provider, id_token) {
+  if (config.allowed_providers.indexOf(provider) == -1) {
+    throw new Error('Unknown Provider')
+  }
+
+  var rcd = yield this.findOne({
+    provider: provider,
+    id_token: id_token
+  });
+
+  if (rcd) {
+    return rcd;
+  }
+}
 
 
 // vim: expandtab:ts=2:sw=2
-
